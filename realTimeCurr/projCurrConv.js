41740c3f696095e2552271ebbf031063d3497a2b
CoolShivas <shivas2710cool00@gmail.com>
2025-04-03 00:37:39 +0530

    Logic is added with functionality.

--- realTimeCurr/projCurrConv.js
+++ realTimeCurr/projCurrConv.js
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import https from "https";
 import readline from "readline";
 
@@ -9,6 +10,11 @@ const lineReader = readline.createInterface({
 
 const apiKey = `7fa642aa79bbfa4e8451c11c`;
 const uRL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+
+
+const convertCurrency = (amount, rate) => {
+    return (amount * rate).toFixed(2); // // .toFixed(2) is used for only two decimal values;
+};
 
 
 https.get(uRL, (response) => {
@@ -24,16 +30,37 @@ https.get(uRL, (response) => {
         // console.log(rate); 
         // // Showing the data on console screen;
         // // Here, we are getting the data from the server;
-        lineReader.question(`Enter the amount in USD : `, () => {
-            lineReader.question(`Enter the target currency (e.g., INR, EUR, NPR) : `, () => {
-                console.log("End of printing Headings.");
+
+        // // amount = 90;
+        // // currency = INR;
+        // // 90USD = how much INR?
+        // // 1USD = 84.9667 INR;
+        // // Therefore, 90USD = ?
+        // // 90 * 84.9667 
+
+
+        lineReader.question(`Enter the amount in USD : `, (amount) => {
+            lineReader.question(`Enter the target currency (e.g., INR, EUR, NPR) : `, (currency) => {
+                const rating = rate[currency.toUpperCase()];
+                if(rating)
+                {
+                    console.log(chalk.blue.bgRed.bold(`${amount} USD is approximately ${convertCurrency(amount, rating)} ${currency}`));
+                }
+                else
+                {
+                    console.log(`Invalid currency code`);
+                }
+                // console.log("End of printing Headings.");
                 lineReader.close(); // // Closing the project execution here;
             });
         });
     });  // // Use of response.on / response.addListener to listen the event 'end' after the data event not responding or server not responding;
 });
 // // // Here, we are getting the Output on Terminal as :-
-// // // Enter the amount in USD : 1
-// // // Enter the target currency (e.g., INR, EUR, NPR) : 2
-// // // End of printing Headings.
+// // // Enter the amount in USD : 2
+// // // Enter the target currency (e.g., INR, EUR, NPR) : inr
+// // // 2 USD is approximately 171.30 inr
 // // // Completed running 'projCurrConv.js'
+
+
+
